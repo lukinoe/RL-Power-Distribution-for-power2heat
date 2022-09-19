bfa22cf87ee0ed7ee0754fe248c5dd2054270106
Lukas Nöbauer <lukas.noebauer@hotmail.com>
2022-09-19 23:36:21 +0200

    a

--- models/DLinear_Merge_Seq2Seq_Timeenc_M.py
+++ models/DLinear_Merge_Seq2Seq_Timeenc_M.py
@@ -76,7 +76,7 @@ class Model(nn.Module):
         self.lstm_hidden = 2048
         hidden = self.lstm_hidden
         self.lstm_n_layers = 1
-        input_size = 8
+        input_size = 10
 
         self.Linear_Merge1 = nn.Linear(self.pred_len*2,hidden)
         self.activation = nn.GELU()
