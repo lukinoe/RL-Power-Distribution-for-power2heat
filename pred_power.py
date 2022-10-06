a829be19fc0692f1af9511a1088d5b40cd8d61e1
Lukas Nöbauer <lukas.noebauer@hotmail.com>
2022-10-06 09:39:32 +0100

    a

--- pred_power.py
+++ pred_power.py
@@ -1,6 +1,6 @@
 import pickle
 import os
-import pandas as pd
+import numpy as np
 from power_model.utils.onehot_encode import onehot_build_dataset # . = relative path
 from power_model.utils.onehot_encode import add_time_features
 
@@ -9,9 +9,11 @@ _dir = os.path.dirname(os.path.realpath(__file__))
 with open(_dir + '/power_model/saved_models/model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-with open(_dir + '/power_model/saved_scaler/scaler.pkl', 'rb') as f:
-    scaler = pickle.load(f)
+with open(_dir + '/power_model/saved_scaler/scaler_y.pkl', 'rb') as f:
+    scaler_y = pickle.load(f)
 
+with open(_dir + '/power_model/saved_scaler/scaler_m1sum.pkl', 'rb') as f:
+    scaler_m1sum = pickle.load(f)
 
 target = "power_consumption"
 
@@ -19,10 +21,13 @@ def predict_power(solar_data):
 
     x = add_time_features(solar_data)
     x = onehot_build_dataset(x, target, del_target=False)
-    print(x.shape)
+    x[:,0] = scaler_m1sum.transform(x[:,0].reshape(-1, 1)).flatten()
 
     y_pred = model.predict(x)
-    y_pred = scaler.inverse_transform(y_pred.reshape(-1, 1))
+    y_pred = scaler_y.inverse_transform(y_pred.reshape(-1, 1))
+    y_pred = y_pred.clip(min=0)
 
     return y_pred
+
+
 
